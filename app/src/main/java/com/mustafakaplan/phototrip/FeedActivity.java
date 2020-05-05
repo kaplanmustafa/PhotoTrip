4c15430046d032a34ed39eb58b2d2810ebd792a7
mustafakaplan <mstfakapln@gmail.com>
2020-05-05 17:29:45 +0300

    Gereksiz import kaldırıldı.

--- app/src/main/java/com/mustafakaplan/phototrip/FeedActivity.java
+++ app/src/main/java/com/mustafakaplan/phototrip/FeedActivity.java
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.app.ActionBar;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
